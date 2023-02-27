6d9daee664a9845cddab52b7c939d6c3c720c1cf
Your Name <fmz@eepa.local>
2023-02-27 15:30:21 -0300

    actualizacion

---
+++ pruebas/calculadora.js
@@ -0,0 +1,20 @@
+function sumar (a, b){
+    return a + b
+}
+console.log(sumar(5,4))
+
+function restar (a, b){
+    return a - b
+}
+console.log(restar(7,8))
+
+function multiplicar (a, b){
+    return a * b
+}
+console.log(multiplicar(5,8))
+
+function dividir (a, b){
+    return a / b
+}
+console.log(dividir(0,0))
+
---
+++ pruebas/ejercicios/loops.js
@@ -0,0 +1,81 @@
+//Task 1
+//Write a "for" loop that will perform exactly the same repetitive code as this:
+
+//console.log(1)
+//console.log(2)
+//console.log(3)
+//console.log(4)
+//console.log(5)
+//console.log('Counting completed!')
+
+for (let i = 1 ; i <=5 ; i++){
+    console.log(i)
+}
+console.log('Counting completed!')
+
+//Task 2 
+//Write a "for" loop that will perform exactly the same repetitive code as this:
+
+//console.log(5)
+//console.log(4)
+//console.log(3)
+//console.log(2)
+//console.log(1)
+//console.log('Countdown finished!')
+
+for (let y = 5 ; y > 0 ; y--){
+    console.log(y)
+}
+console.log('Countdown finished!')
+
+//Task 3
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+
+//console.log(1)
+//console.log(2)
+//console.log(3)
+//console.log(4)
+//console.log(5)
+//console.log('Counting completed!')
+
+let x = 1
+while (x < 6 ){
+    console.log(x)
+    x++
+}
+console.log('Counting completed!')
+
+//Task 4
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+
+//console.log(5)
+//console.log(4)
+//console.log(3)
+//console.log(2)
+//console.log(1)
+//console.log('Countdown finished!')
+
+let z = 5
+while (z > 0){
+    console.log(z)
+    z--
+}
+console.log('Countdown finished!')
+
+//Task 5
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+
+//console.log(2018)
+//console.log(2019)
+//console.log(2020)
+//console.log(2021)
+//console.log(2022)
+//console.log(2023)
+
+let year = 2018
+while (year < 2023){
+    console.log(year)
+    year++
+}
+console.log(year)
+
